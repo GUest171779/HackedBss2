b2d36ba5667c90458c0d311aeb140c210a208612
GUest171779 <131062813+GUest171779@users.noreply.github.com>
2025-01-17 16:51:14 -0500

    Update playergear.js

--- playergear.js
+++ playergear.js
@@ -92,10 +92,10 @@ window.playerGear={
         
         superSaturator:{
 
-            count:1,
-            diameter:15,
-            power:0.5,
-            rate:1,
+            count:5,
+            diameter:45,
+            power:1,
+            rate:0.1,
             mesh:function(x,y,z,box,cylinder){
 
                 cylinder(x,y+0.25,z,0.15,2.5,10,0.9,0.9,0.5,1,90,0,0)
