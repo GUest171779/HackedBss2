3f14a1db96b1c7347b646af1d6b3590c6ab5d62d
GUest171779 <131062813+GUest171779@users.noreply.github.com>
2025-01-19 11:01:26 -0500

    Update Armor.js

--- Armor.js
+++ Armor.js
@@ -983,8 +983,8 @@ window.playerGear={
                 player.addEffect('bubbleBombsPassive')
                 player.addEffect('petalStormPassive')
                 stats.movementCollection+=9999999999
-                stats.walkSpeed*=2.8
-                stats.jumpPower*=2.4
+                stats.walkSpeed*=3.5
+                stats.jumpPower*=3.5
                 stats.pollenFromCoconuts*=99999
                 stats.beeAttack+=9999999999
                 stats.convertRate*=999999999
